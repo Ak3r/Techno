51cd784236425d8865e305750afcef950fed72d9
andrew <andrew.kostyukevich@gmail.com>
2021-09-30 12:10:02 +0300

    event loop

---
+++ hm4/hm4.js
@@ -0,0 +1,123 @@
+// 1. Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+
+// Используя setInterval.
+function printNumbers(from, to) {
+  
+  let intervalId = setInterval(function() {
+    console.log(from);
+    if (from === to) {
+      clearInterval(intervalId);
+    }
+    from++;
+  }, 1000);
+}
+
+printNumbers(1, 5);
+
+// Используя рекурсивный setTimeout.
+function printNumbers(from, to) {
+
+  setTimeout(function count() {
+    console.log(from);
+    if (from < to) {
+      setTimeout(count, 1000);
+    }
+    from++;
+  }, 1000);
+}
+
+printNumbers(1, 5);
+
+// 2. Напишите часы с использованием setInterval (с выводом в консоль), при каждом новом выводе (каждую секунду) очищать сонсоль (console.clear)
+
+function clock() {
+  setTimeout(function tick() {
+
+    console.clear();
+
+    let now = new Date();
+    console.log(now.toLocaleTimeString());
+
+    setTimeout(tick, 1000);
+
+  }, 1000);
+}
+
+clock();
+
+// *3. Результатом декоратора debounce(f, ms) должна быть обёртка, которая передаёт вызов f не более одного раза в ms миллисекунд. Другими словами, 
+// когда мы вызываем debounce, это гарантирует, что все остальные вызовы будут игнорироваться в течение ms.
+
+function debounce(f, ms) {
+
+  let isCooldown = false;
+
+  return function() {
+    if (isCooldown) return;
+
+    f.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => isCooldown = false, ms);
+  };
+
+}
+
+let f = debounce(alert, 1000);
+
+f(1); // выполняется немедленно
+f(2); // проигнорирован
+
+setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
+setTimeout( () => f(4), 1100); // выполняется
+setTimeout( () => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
+
+// *4. Создайте «тормозящий» декоратор throttle(f, ms), который возвращает обёртку, передавая вызов в f не более одного раза в ms миллисекунд.
+// Те вызовы, которые попадают в период «торможения», игнорируются.
+
+// Отличие от debounce – если проигнорированный вызов является последним во время «задержки», то он выполняется в конце.
+
+function throttle(func, ms) {
+
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+function f(a) {
+  console.log(a)
+}
+
+// f1000 передаёт вызовы f максимум раз в 1000 мс
+let f1000 = throttle(f, 1000);
+
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
+
+// когда 1000 мс истекли ...
+// ...выводим 3, промежуточное значение 2 было проигнорировано
