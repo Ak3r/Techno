444a50849b27d54f395bdc6c40b640a58cce75ff
andrew <andrew.kostyukevich@gmail.com>
2021-10-28 15:18:46 +0300

    initial

---
+++ hm6/.eslintrc.js
@@ -0,0 +1,19 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
